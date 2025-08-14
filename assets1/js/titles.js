1ddaa455054e9c5ac868228704f9b30dfb0354ac
fxlsealiet <skibidisigmaiscool97@gmail.com>
2025-08-13 23:26:25 -0400

    Update titles.js

--- assets1/js/titles.js
+++ assets1/js/titles.js
@@ -1,2 +1,2 @@
-const rrr=["seal","biolink!!","https://discord.gg/SBwgMWXATW"];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
+const rrr=["seal","biolink!!","/SBwgMWXATW"];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
 setInterval(yyy,250)
